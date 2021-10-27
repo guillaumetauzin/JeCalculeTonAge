b4c9998d7cf837e4e7e8d23b28f9dfa1d1ae21ee
Guillaume <36336159+guillaumetauzin@users.noreply.github.com>
2021-10-27 09:57:36 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+package com.company;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Quelle est ton année de naissance ? (dd/MM/yyyy): ");
+
+        String date = br.readLine();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        LocalDate naissance = LocalDate.parse(date, formatter);
+        LocalDate actuel = LocalDate.now();
+
+        Period period = Period.between(naissance, actuel);
+        System.out.print("Tu as donc ");
+        System.out.print(period.getYears());
+        System.out.println(" ans");
+    }
+}
